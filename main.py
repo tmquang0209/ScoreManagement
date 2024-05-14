1f3f1c1c75cd37d5a3f5271a89413f2a8f13111b
Trần Minh Quang <tmquang0209@gmail.com>
2024-05-14 22:53:12 +0700

    add main screen

---
+++ main.py
@@ -0,0 +1,65 @@
+from tkinter import *
+from tkinter.ttk import *
+from screens.home import Home
+from screens.login import Login
+from screens.years import Years
+
+import modules.localStorage as localStorage
+
+class ScoreApp(Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Hệ thống quản lý đào tạo")
+
+        self.localStorage = localStorage
+
+        self.token = self.checkToken()
+
+        # Initial screen
+        self.currentScreen = self.getInitialScreen()
+
+        # Initialize container for stacking screens
+        self.container = Frame(self, width=800, height=500)
+        self.container.pack(side="top", fill="both", expand=True)
+        self.container.grid_rowconfigure(0, minsize=500, weight=2)
+        self.container.grid_columnconfigure(0, minsize=600, weight=2)
+
+        self.screens = {}
+        for F in (Home, Login, Years):
+            pageName = F.__name__
+            frame = F(parent=self.container, controller=self)
+            self.screens[pageName] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.showFrame(self.currentScreen)
+        self.displayMenu()
+
+    def displayMenu(self):
+        if self.token:
+            from modules.menu import MenuManager
+            menuManager = MenuManager(controller=self)
+            menu = menuManager.createMenu()
+            self.config(menu=menu)
+        else:
+            self.config(menu=None)
+
+    def checkToken(self):
+        token = self.localStorage.getItem("token")
+        if token:
+            return token
+        else:
+            return None
+
+    def getInitialScreen(self):
+        if self.token != None:
+            return "Home"
+        else:
+            return "Login"
+
+    def showFrame(self, pageName):
+        self.screens[pageName].tkraise()
+
+# Run the application
+if __name__ == "__main__":
+    app = ScoreApp()
+    app.mainloop()
